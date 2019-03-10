98a70a0daa2356c89cc921b6aa28e3a384ebe3aa
Zhu Zhengxing <haina.z@163.com>
2019-03-10 19:48:02 +0800

    Update main.c

--- main.c
+++ main.c
@@ -3,7 +3,10 @@ void main ( void )
 {
     unsigned char u8Cnt;
 
-    for ( u8Cnt = 0; u8Cnt < 100; u8Cnt++ )
+    for ( u8Cnt = 0;
+          u8Cnt < 100;
+          u8Cnt++
+        )
     {
     }
 }
